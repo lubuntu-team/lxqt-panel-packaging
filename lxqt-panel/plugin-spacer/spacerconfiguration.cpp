ef4c5af2a31d9bfe296894b595689a112e3c1d3f
Andrew Lee (李健秋) <ajqlee@debian.org>
2015-08-21 22:50:31 +0800

    Adding upstream version 0.9.0+20150720.
    Signed-off-by: Andrew Lee (李健秋) <ajqlee@debian.org>

---
+++ lxqt-panel/plugin-spacer/spacerconfiguration.cpp
@@ -0,0 +1,77 @@
+/* BEGIN_COMMON_COPYRIGHT_HEADER
+ * (c)LGPL2+
+ *
+ * LXDE-Qt - a lightweight, Qt based, desktop toolset
+ * http://lxqt.org
+ *
+ * Copyright: 2015 LxQt team
+ * Authors:
+ *
+ * This program or library is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA
+ *
+ * END_COMMON_COPYRIGHT_HEADER */
+
+#include "spacerconfiguration.h"
+#include "ui_spacerconfiguration.h"
+
+
+//Note: strings can't actually be translated here (in static initialization time)
+//      the QT_TR_NOOP here is just for qt translate tools to get the strings for translation
+const QStringList SpacerConfiguration::msTypes = {
+    QStringLiteral(QT_TR_NOOP("lined"))
+    , QStringLiteral(QT_TR_NOOP("dotted"))
+    , QStringLiteral(QT_TR_NOOP("invisible"))
+};
+
+SpacerConfiguration::SpacerConfiguration(QSettings *settings, QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::SpacerConfiguration)
+    , mSettings(settings)
+{
+    setAttribute(Qt::WA_DeleteOnClose);
+    setObjectName("SpacerConfigurationWindow");
+    ui->setupUi(this);
+
+    //Note: translation is needed here in runtime (translator is attached already)
+    for (auto const & type : msTypes)
+        ui->typeCB->addItem(tr(type.toStdString().c_str()), type);
+
+    loadSettings();
+
+    connect(ui->sizeSB, static_cast<void (QSpinBox::*)(int)>(&QSpinBox::valueChanged), this, &SpacerConfiguration::sizeChanged);
+    connect(ui->typeCB, static_cast<void (QComboBox::*)(int index)>(&QComboBox::currentIndexChanged), this, &SpacerConfiguration::typeChanged);
+}
+
+SpacerConfiguration::~SpacerConfiguration()
+{
+    delete ui;
+}
+
+void SpacerConfiguration::loadSettings()
+{
+    ui->sizeSB->setValue(mSettings->value("size", 8).toInt());
+    ui->typeCB->setCurrentIndex(ui->typeCB->findData(mSettings->value("spaceType", msTypes[0]).toString()));
+}
+
+void SpacerConfiguration::sizeChanged(int value)
+{
+    mSettings->setValue("size", value);
+}
+
+void SpacerConfiguration::typeChanged(int index)
+{
+    mSettings->setValue("spaceType", ui->typeCB->itemData(index, Qt::UserRole));
+}
---
+++ lxqt-panel/plugin-worldclock/lxqtworldclockconfiguration.h
@@ -0,0 +1,106 @@
+/* BEGIN_COMMON_COPYRIGHT_HEADER
+ * (c)LGPL2+
+ *
+ * LXDE-Qt - a lightweight, Qt based, desktop toolset
+ * http://razor-qt.org
+ *
+ * Copyright: 2012 Razor team
+ * Authors:
+ *   Kuzma Shapran <kuzma.shapran@gmail.com>
+ *
+ * This program or library is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA
+ *
+ * END_COMMON_COPYRIGHT_HEADER */
+
+
+#ifndef LXQT_PANEL_WORLDCLOCK_CONFIGURATION_H
+#define LXQT_PANEL_WORLDCLOCK_CONFIGURATION_H
+
+#include <LXQt/Settings>
+
+#include <QDialog>
+#include <QAbstractButton>
+#include <QFont>
+#include <QMap>
+
+
+namespace Ui {
+    class LxQtWorldClockConfiguration;
+}
+
+class LxQtWorldClockConfigurationTimeZones;
+class LxQtWorldClockConfigurationManualFormat;
+class QTableWidgetItem;
+
+class LxQtWorldClockConfiguration : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit LxQtWorldClockConfiguration(QSettings *settings, QWidget *parent = NULL);
+    ~LxQtWorldClockConfiguration();
+
+public slots:
+    void saveSettings();
+
+private:
+    Ui::LxQtWorldClockConfiguration *ui;
+    QSettings *mSettings;
+    LxQt::SettingsCache mOldSettings;
+
+    /*
+      Read settings from conf file and put data into controls.
+    */
+    void loadSettings();
+
+private slots:
+    /*
+      Saves settings in conf file.
+    */
+    void dialogButtonsAction(QAbstractButton *);
+
+    void timeFormatChanged(int);
+    void dateGroupToggled(bool);
+    void dateFormatChanged(int);
+    void advancedFormatToggled(bool);
+    void customiseManualFormatClicked();
+    void manualFormatChanged();
+
+    void updateTimeZoneButtons();
+    void addTimeZone();
+    void removeTimeZone();
+    void setTimeZoneAsDefault();
+    void editTimeZoneCustomName();
+    void moveTimeZoneUp();
+    void moveTimeZoneDown();
+
+private:
+    QString mDefaultTimeZone;
+
+    bool mLockCascadeSettingChanges;
+
+    LxQtWorldClockConfigurationTimeZones *mConfigurationTimeZones;
+    LxQtWorldClockConfigurationManualFormat *mConfigurationManualFormat;
+
+    QString mManualFormat;
+
+    void setDefault(int);
+    void setBold(QTableWidgetItem*, bool);
+    void setBold(int row, bool value);
+    int findTimeZone(const QString& timeZone);
+};
+
+#endif // LXQT_PANEL_WORLDCLOCK_CONFIGURATION_H
